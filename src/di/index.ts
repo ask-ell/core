41408b7ea653c1931f93ffb4f326adfef56ecc45
Yannick Debrée <yannickdebree@ask-ell.com>
2024-02-02 00:51:23 +0100

    "Antheme" core added (#6)

---
+++ src/di/index.ts
@@ -0,0 +1,5 @@
+import Container from "./container";
+import Inject from "./inject";
+import Injectable from "./injectable";
+
+export { Container, Inject, Injectable };
---
+++ src/primitives/index.ts
@@ -0,0 +1,46 @@
+import type EntityProvider from "./oop/entity/entity.provider";
+import type EntityRepository from "./oop/entity/entity.repository";
+import Entity from "./oop/entity/entity";
+import { append, find } from "./fp/array";
+import { _if } from "./fp/boolean";
+import { log } from "./fp/console";
+import { left, matchEither, right } from "./fp/either";
+import { identity, pipe } from "./fp/function";
+import { io } from "./fp/io";
+import type { Maybe, MaybeNullOrUndefined, MaybeUndefined } from "./fp/maybe";
+import { fromNullableOption, matchOption, none, some } from "./fp/options";
+import { isEmptyString, length, string, stringToNumber } from "./fp/string";
+import type Equalizable from "./oop/equalizable";
+import type Stringable from "./oop/stringable";
+
+export type {
+  EntityProvider,
+  EntityRepository,
+  Maybe,
+  MaybeNullOrUndefined,
+  MaybeUndefined,
+  Equalizable,
+  Stringable,
+};
+
+export {
+  Entity,
+  append,
+  find,
+  _if,
+  log,
+  left,
+  matchEither,
+  right,
+  identity,
+  pipe,
+  io,
+  fromNullableOption,
+  matchOption,
+  some,
+  isEmptyString,
+  length,
+  string,
+  stringToNumber,
+  none,
+};
